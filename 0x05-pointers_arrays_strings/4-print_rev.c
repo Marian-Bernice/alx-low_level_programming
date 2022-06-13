9e0c0a4c8b4ee11c261272ced7d1e0a81fe70dd3
Marian-Bernice <marian.haligah@ashesi.edu.gh>
2022-06-13 02:25:04 -0700

    new commit

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -3,10 +3,11 @@
  * print_rev - prints reversed string, followed by a new line
  * @s: pointer to the string to print
  * Return: void
- */
+*/
 
 void print_rev(char *s)
 {
+
 	int i = 0;
 	
 	while (s[i])
