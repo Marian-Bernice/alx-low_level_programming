8017c144012efbf64c9ee214c286bd52f0745539
Marian-Bernice <marian.haligah@ashesi.edu.gh>
2022-06-17 00:41:15 -0700

    new commit

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -18,12 +18,10 @@ char *leet(char *c)
 		for (i = 0; i < sizeof(key) / sizeof(char); i++)
 		{
 			/*32 is the difference between lower case letters and apper case letters*/
-
 			if (*c == key[i] || *c == key[i] + 32)
-			{		
+			{
 				*c = 48 + value[i];
 			}
-
 		}
 		c++;
 	}
