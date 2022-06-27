5b72dad5c444a67f27bef26886298810752fa52f
Marian-Bernice <marian.haligah@ashesi.edu.gh>
2022-06-27 01:44:58 -0700

    new commit

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,36 +1,47 @@
 #include <stdlib.h>
 
 /**
- * argstostr - Concatenates all the arguments of the program
- * @ac: Argument total count
- * @av: Pointer to arguments
- * Retunr: Pointer to concatenated string (SUCCESS) or
- * NULL if @ac == 0 or @av == NULL (FAILURE) or
- * NULL if if insufficient memory was available (FAILURE)
+ * argstostr - a function that concatenates
+ * all the arguments
+ * @ac: argument counter
+ * @av: argument holder
+ * Return: a pointer to a new string
+ * or NULL if it fails
  */
 
 char *argstostr(int ac, char **av)
 {
-	int i, j;
-	int count = 0;
-	int t_count = 0;
-	char *result;
+	int i, j, k, length;
+	char *str;
 
 	if (ac == 0 || av == NULL)
-		return ('\0');
+		return (NULL);
 
+	/*find length of vector + '\0' which makes it a 2d array*/
+	length = 0;
 	for (i = 0; i < ac; i++)
 	{
 		for (j = 0; av[i][j] != '\0'; j++)
-			t_count++;
-
-		t_count++;
+			length++;
+		length++;
 	}
 
-	result = malloc(sizeof(char) * t_count + 1);
+	str = malloc((length + 1) * sizeof(char));
+	if (str == NULL)
+		return (NULL);
 
-	if (result == NULL)
+	k = 0;
+	for (i = 0; i < ac; i++)
 	{
-		return ('\0');
+		for (j = 0; av[i][j] != '\0'; j++)
+		{
+			str[k] = av[i][j];
+			k++;
+		}
+		str[k] = '\n';
+		k++;
 	}
+	str[k] = '\0';
 
+	return (str);
+}
