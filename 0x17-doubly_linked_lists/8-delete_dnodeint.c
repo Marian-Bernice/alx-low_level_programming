950e9289bd481af9ece4a735f605d2109c98febe
Marian-Bernice <marian.haligah@ashesi.edu.gh>
2022-09-01 06:10:33 -0700

    new commit

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -1,41 +1,39 @@
 #include "lists.h"
-
 /**
- * delete_dnodeint_at_index - delete node at specific index
- * @head: head of linked list
- * @index: index of node to delete
- * Return: 1 on success, -1 on failure
+ * delete_dnodeint_at_index - deletes node at given index
+ * @head: pointer to beggining of a list
+ * @index: the index to at which to insert
+ * Return: 1 on succes, or -1 if it failed
  */
-
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-	dlistint_t *current;
+	dlistint_t *tmp;
+	unsigned int counter = 0;
 
-	if (head == NULL)
+	if (head == NULL || *head == NULL)
 		return (-1);
-
-	current = *head;
-
-	if (current == NULL)
-		return (-1);
-
-	while (index != 0)
+	tmp = *head;
+	if (index == 0)
 	{
-		current = current->next;
-		index--;
-		if (current == NULL)
+		*head = tmp->next;
+		if (tmp->next != NULL)
+		{
+			tmp->next->prev = NULL;
+		}
+		free(tmp);
+		return (1);
+	}
+
+	while (counter < index)
+	{
+		if (tmp->next == NULL)
 			return (-1);
+		tmp = tmp->next;
+		counter++;
 	}
-
-	if (current->prev != NULL)
-		current->prev->next = current->next;
-	else
-	{
-		*head = current->next;
-		if (current->next != NULL)
-			current->next->prev = NULL;
-		free(current);
-	}
-
+	tmp->prev->next = tmp->next;
+	if (tmp->next != NULL)
+		tmp->next->prev = tmp->prev;
+	free(tmp);
 	return (1);
 }
